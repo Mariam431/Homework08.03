6a7e95d537fea1a73e38be8def56173617f240c7
mariam <mariam@MYPROBOOK>
2023-03-08 16:26:54 +0400

    Add project files.

---
+++ Homework08.03/Parent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework08._03
+{
+    public class Parent
+    {
+        //All access modifiers
+        public int Public = 10;
+        private int Private = 20;
+        protected int Protected = 30;
+        internal int Internal = 40;
+        protected internal int ProtectedInternal = 50;
+
+        public string name;
+    }
+    public class Child : Parent
+    {
+        public Child()
+        {
+            Public *= 10;
+            //Private *= 10;
+            Protected *= 10;
+            Internal *= 10;
+            ProtectedInternal *= 10;
+
+            name = "bob";
+        }
+    }
+}
+
+
+
+
---
+++ Homework08.03/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework08._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            Console.WriteLine(child.name);
+            Console.WriteLine(child.Public);
+            Console.WriteLine(child.Internal);
+            Console.WriteLine(child.ProtectedInternal);
+            //Console.WriteLine(child.Private);
+            //Console.WriteLine(child.Protected);
+
+
+        }
+    }
+}
